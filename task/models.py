f6417651582899e404de5933851a8432d07d0a7d
anup-mirkute <anupmirkute1008@gmail.com>
2023-03-27 23:07:06 +0530

    Add Functionalities

--- task/models.py
+++ task/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField(max_length=128)
--- task/views.py
+++ task/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import Detail, Address
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
+    details = User.objects.all()
+    userform = Detail
+    addressform = Address
+    context = {
+        'details': details,
+        'userform':userform,
+        'addressform':addressform,
+    }
+    return render(request, 'index.html', context)
+
+def addDetails(request):
+    
     return render(request, 'index.html')
